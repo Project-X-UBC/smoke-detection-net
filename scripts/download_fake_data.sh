b68461e505b7db18d077348ebfe64af0a286a2eb
rdesc <rodrigue.de.schaetzen@gmail.com>
2020-10-16 07:27:26 +0000

    fixes and makes scripts more robust + adds new key to config

--- scripts/download_fake_data.sh
+++ scripts/download_fake_data.sh
@@ -2,12 +2,13 @@
 
 sudo apt install unzip
 
+data_path=../data/synthetic
 # download the fake data
-gdown https://drive.google.com/uc?id=1V2xcLqkyJKqfAzUO7C5bUceyQtq0BjNe -O data/synthetic/fake_data.zip
-unzip data/synthetic/fake_data.zip -d data/synthetic
-mv fake_data/* data/synthetic
-rm -rf fake_data
-rm -rf data/synthetic/__MACOSX/
+gdown https://drive.google.com/uc?id=1V2xcLqkyJKqfAzUO7C5bUceyQtq0BjNe -O $data_path/fake_data.zip
+unzip $data_path/fake_data.zip -d $data_path
+mv $data_path/fake_data/* $data_path
+rm -rf $data_path/fake_data
+rm -rf $data_path/__MACOSX/
 
 # make the json files for metadata
 exec ./generate_metadata_jsons.sh data/synthetic
