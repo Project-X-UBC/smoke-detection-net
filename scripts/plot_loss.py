5ae41c724752d4bfb42a0181e7caa2a4179d9741
rdesc <rodrigue.de.schaetzen@gmail.com>
2020-10-19 20:02:05 +0000

    adds proper training time to loss plot title

--- scripts/plot_loss.py
+++ scripts/plot_loss.py
@@ -1,4 +1,5 @@
 import os
+import re
 import jsonlines
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,17 +13,19 @@ def plot_loss(output_dir):
             if 'total_loss' in obj.keys():
                 data.append(obj)
 
+    log = open(os.path.join(output_dir, "log.txt"), 'r')
+    training_time = re.search('time:(.+?) \(', log.readlines()[-1]).group(1)
+
     df = pd.json_normalize(data)
     plt.plot(df['iteration'], df['total_loss'], label='total loss')
-    plt.plot(df['iteration'], df['validation_loss'], label='validation loss')
+    plt.plot(df['iterlsation'], df['validation_loss'], label='validation loss')
     plt.xlabel('iteration #, 1 epoch = %i iterations' %
                compute_params(os.path.join(output_dir, 'config.yaml'))['one_epoch'])
     plt.legend()
-    total_hours = (df['eta_seconds'].max() - df['eta_seconds'].min())/60/60
-    plt.title("Model loss, total training time %.2f hours" % total_hours)
+    plt.title("Model loss, total training time%s hours" % training_time)
     plt.savefig(os.path.join(output_dir, 'model_loss.png'))
 
 
 if __name__ == '__main__':
-    output = "output/test"
+    output = "../output/output_dir"
     plot_loss(output)
